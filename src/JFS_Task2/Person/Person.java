92376f545ddf8e7ac57460aa60b3238c48d01387
Anbu-Saravanan <anbu.saravanan11211@gmail.com>
2025-04-19 21:05:38 +0530

    second commit

---
+++ src/JFS_Task2/Person/Person.java
@@ -0,0 +1,28 @@
+package JFS_Task2.Person;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name){
+        this.name=name;
+        age = 18;
+    }
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+    public void display(){
+        System.out.println("Person name is: "+ name + "\nAge is: "+ age);
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Saravanan");
+        Person person1 = new Person("Alex",36);
+
+        person.display();
+        System.out.println("-----");
+        person1.display();
+    }
+}
