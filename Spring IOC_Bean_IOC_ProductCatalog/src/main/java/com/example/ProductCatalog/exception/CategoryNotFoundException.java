9b285cd42099141545650c086b8fb56ff6d9ad2c
Anbu-Saravanan <anbu.saravanan11211@gmail.com>
2025-06-11 16:43:56 +0530

    Delete Spring IOC_Bean_IOC_ProductCatalog directory

--- Spring IOC_Bean_IOC_ProductCatalog/src/main/java/com/example/ProductCatalog/exception/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProductCatalog.exception;
-
-public class CategoryNotFoundException extends RuntimeException{
-    public CategoryNotFoundException(String category) {
-        super("No product found in category "+ category);
-    }
-}
--- Spring IOC_Bean_IOC_ProductCatalog/src/main/java/com/example/ProductCatalog/exception/SearchIdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProductCatalog.exception;
-
-public class SearchIdNotFoundException extends RuntimeException{
-    public SearchIdNotFoundException(Integer id) {
-        super("Your product's ID: "+id+" doesn't exist");
-    }
-}
